d84f2ad61848c64f4a8e1b5308b95351b5bf517e
jinsoo960 <jinihm@umich.edu>
2016-10-07 14:35:48 -0400

    Added divide for calculate

--- rpn.py
+++ rpn.py
@@ -18,6 +18,11 @@ def calculate(input):
 			arg2 = stack.pop()
 			result = arg1 * arg2
 			stack.append(result)
+		elif token == '/':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg2 / arg1
+			stack.append(result)
 		else:
 			stack.append(int(token))
 
